febc379f52a7b3613e7433719859ca641062893d
이진경 <49181825+EJinK0@users.noreply.github.com>
2025-08-09 20:49:25 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+        System.out.print(sb.toString());
+    }
+}
