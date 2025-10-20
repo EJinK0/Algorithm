6f58902ba1465bc0e49fad1b7b2e07d40c9fae84
이진경 <49181825+EJinK0@users.noreply.github.com>
2025-10-20 23:14:26 +0900

    [Silver IV] Title: 수 찾기, Time: 616 ms, Memory: 42992 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<M;i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(Arrays.binarySearch(arr, num) >= 0) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
