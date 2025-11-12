dc4822ed22c7c57cddeda7d3321ae19e7b6f672c
이진경 <49181825+EJinK0@users.noreply.github.com>
2025-11-12 19:53:33 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine().trim());
+        long sum = n * (n + 1) / 2;
+        System.out.println(sum);
+    }
+}
