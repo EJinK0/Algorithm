0e0f0afda0f1e87e77fdc914c377b1a0c9430367
이진경 <49181825+EJinK0@users.noreply.github.com>
2025-11-14 00:01:58 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    static long factorial(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+    }
+}
