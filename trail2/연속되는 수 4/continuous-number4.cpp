2b1cf61798850aef73f9238d5807acfb02ba73ad
이진경 <49181825+EJinK0@users.noreply.github.com>
2026-09-13 18:52:37 +0900

    [Passed] 연속되는 수 4 (2ms, 0MB)

---
+++ trail2/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int count=1;
+    int answer=1;
+    for(int i=1;i<N;i++) {
+        if(A[i] > A[i-1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+
+        answer = max(answer, count);
+    }
+
+    cout << answer;
+    // Please write your code here.
+
+    return 0;
+}
