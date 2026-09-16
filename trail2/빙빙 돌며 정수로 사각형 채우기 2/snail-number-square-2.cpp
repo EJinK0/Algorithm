1587bb81c2dfdd6b41738a4f9b7966cb5889ab7f
이진경 <49181825+EJinK0@users.noreply.github.com>
2026-09-16 22:33:43 +0900

    [Passed] 빙빙 돌며 정수로 사각형 채우기 2 (1ms, 0MB)

---
+++ trail2/빙빙 돌며 정수로 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+
+    // 하우상좌
+    int dr[4] = {1,0,-1,0};
+    int dc[4] = {0,1,0,-1};
+    
+    vector<vector<int>> board(n, vector<int>(m,0));
+
+    int r=0;
+    int c=0;
+    int dir=0;
+
+    board[r][c] = 1;
+
+    for(int num=2;num<=n*m;num++) {
+        int nr=r+dr[dir];
+        int nc=c+dc[dir];
+
+        if(nr<0 ||nr>=n||nc<0||nc>=m||board[nr][nc] != 0) {
+            dir=(dir+1)%4;
+            nr=r+dr[dir];
+            nc=c+dc[dir];
+        }
+
+        r=nr;
+        c=nc;
+
+        board[nr][nc] = num;
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
